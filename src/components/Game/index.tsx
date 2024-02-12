51c6531700d4659944ea0b65e38194f40982e34d
Ryan Hammer <ryanhammer1@protonmail.com>
2024-02-11 20:02:59 -0500

    add guess component to game

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,6 +1,10 @@
-import { sample } from '../../utils';
+import { useState } from 'react';
+import { range, sample } from '../../utils';
 import { WORDS } from '../../data';
+import { PreviousGuess } from '../../types';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import GuessForm from './GuessForm';
+import Guess from './Guess';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -8,9 +12,18 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+  const [previousGuesses, setPreviousGuesses] = useState<PreviousGuess[]>([]);
+
+  const guesses = range(0, NUM_OF_GUESSES_ALLOWED).map((val) => val + 1);
+
   return (
     <>
-      <GuessForm />
+      <div className='flex flex-col justify-center mb-4'>
+        {guesses.map((guessNumber) => {
+          return <Guess key={guessNumber} previousGuesses={previousGuesses} guessNumber={guessNumber} />;
+        })}
+      </div>
+      <GuessForm previousGuesses={previousGuesses} setPreviousGuesses={setPreviousGuesses} />
     </>
   );
 }
