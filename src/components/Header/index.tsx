ade75183462701c57a042d12fe0b751e89dba460
Ryan Hammer <ryanhammer1@protonmail.com>
2024-02-11 20:02:45 -0500

    cleanup

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-
-function Header() {
+export default function Header() {
   return (
-    <header className='flex h-16 border-b border-b-gray-300 text-gray-700'>
+    <header className='flex h-16 border-b border-b-gray-300 text-gray-700 mb-4'>
       <h1>Word Game</h1>
     </header>
   );
 }
-
-export default Header;
